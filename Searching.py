1ca69c7b7a5b228e80e50c93cb13a381ff974af4
james <james>
2018-11-24 17:22:10 +0000

    Add a binary search using recursions

--- Searching.py
+++ Searching.py
@@ -19,7 +19,7 @@ def binarySearch(aList, item):
     if item == midValue:
         return True
     elif item > midValue:
-        subList = aList[half:]
+        subList = aList[half+1:]
     else:
         subList = aList[:half]
     for i in subList:
@@ -27,11 +27,52 @@ def binarySearch(aList, item):
             return True
     return found
 
-if __name__ == "__main__":
+def binarySearchRecursion(aList, item):
+    # Binary search implemented via recursions    
+    # Base case is when the item is not in aList, return false
+    if len(aList) == 0:
+        return False
+    else:
+        # Reduce the aList size by half until it is length of 1 or 0 when item is not in list
+        midpoint = len(aList)//2
+        if aList[midpoint] == item:
+            return True
+        else:
+            if item > aList[midpoint]:
+                return binarySearchRecursion(aList[midpoint+1:], item)
+            else:
+                return binarySearchRecursion(aList[:midpoint], item)
+
+def testLinerSearch():
+    print("[x] Test Liner Search:")
     testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
     print(sequentialSearch(testlist, 3))
     print(sequentialSearch(testlist, 13))
 
+def testBinarySearch():
+    print("[x] Test Binary Search:")
     testlist2 = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
-    print(binarySearch(testlist2, 2))
-    print(binarySearch(testlist2, 33))
+    print(binarySearch(testlist2, 3))
+    print(binarySearch(testlist2, 13))
+
+def testBinarySearchRecursion():
+    print("[x] Test Binary Search via Recursions:")
+    testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+    print(binarySearchRecursion(testlist, 3))
+    print(binarySearchRecursion(testlist, 13))
+    print(binarySearchRecursion(testlist, 44))
+
+def testSubList():
+    testlist = [0, 1, 2, 8, 13, 17, 19, 32,]
+    half = len(testlist) //2
+    print(half)
+    print(testlist[half])
+    print(testlist[:half])
+    print(testlist[half:])
+
+if __name__ == "__main__":
+    #testSubList()    
+    testLinerSearch()
+    testBinarySearch()
+    testBinarySearchRecursion()
+    
